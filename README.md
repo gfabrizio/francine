59253417c60daf5f8269a4fa1216b90ba005759a
Guilherme Fabrizio <gfabricio@gmail.com>
2024-02-01 14:11:58 -0300

    Improve click commands (#4)
    * move cli.py
    
    * general improves
    
    * fix test

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# francine
+# Francine - Daily todo list
+
+Francine is a command-line tool designed to help you keep track of your daily tasks,
+always focusing on the first one inserted.
+
+## Usage
+
+![](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)
+
+## Installing
+
+```bash
+pip install francine
+```
--- francine/cli.py
+++ francine/cli.py
@@ -8,19 +8,31 @@ service = TaskService(repository)
 
 
 @click.group()
-def cli():
+def francine():
+    """Francine helps you to track your short daily goals on track"""
     pass
 
 
 @click.command()
 @click.argument("description")
 def add(description):
+    """# adds a task for your list (francine add "task")"""
+    service.flush()
     service.add(description)
     click.echo(f"Task added: {description}")
 
 
 @click.command()
+def get():
+    """# shows the current task"""
+    task = service.get()
+    click.echo(f"Task: {task.description}")
+
+
+@click.command()
 def list():
+    """# show all your last 24 hours task"""
+    service.flush()
     tasks = service.list()
     for number, task in enumerate(tasks):
         click.echo(f"{number} - {task.description}")
@@ -28,20 +40,15 @@ def list():
 
 @click.command()
 def complete():
+    """# completes the current task"""
     service.complete()
     click.echo("Task completed")
 
 
-@click.command()
-def flush():
-    service.flush()
-    click.echo("Old tasks flushed")
-
-
-cli.add_command(add)
-cli.add_command(list)
-cli.add_command(complete)
-cli.add_command(flush)
+francine.add_command(add)
+francine.add_command(get)
+francine.add_command(list)
+francine.add_command(complete)
 
 if __name__ == "__main__":
-    cli()
+    francine()
